7db73dcf903113724540d357994b07274d135ef6
Dmitry Osipenko <eympel@gmail.com>
2020-12-31 10:49:08 +0300

    fix: fix host to DB

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'rzzxlumzbkvnyv',
     password: '822f994c6fb9ae5146f9a57714028d3d3b5a0367da15524eaafd486aedac5d06',
-    host: 'ec2-176-34-114-78.eu-west-1.compute.amazonaws.com',
+    host: 'localhost',
     port: 5432,
     database: 'd3rt1htds93egi',
     ssl: false
