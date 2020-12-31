8d511938813d7f145e45f23edcf832f229a917a2
Dmitry Osipenko <eympel@gmail.com>
2020-12-31 11:47:05 +0300

    fix: fix uld db

--- db/db.js
+++ db/db.js
@@ -3,9 +3,9 @@ const pool = new Pool({
     user: 'rzzxlumzbkvnyv',
     password: '822f994c6fb9ae5146f9a57714028d3d3b5a0367da15524eaafd486aedac5d06',
     host: 'postgres://',
-    // port: 5432,
-    // database: 'd3rt1htds93egi',
-    // ssl: false
+    port: 5432,
+    database: 'd3rt1htds93egi',
+    ssl: false
 })
 
 module.exports = pool;
