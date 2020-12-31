2b7924e9b3336b0910ddb5da14a90f74c71ff063
Dmitry Osipenko <eympel@gmail.com>
2020-12-31 12:09:29 +0300

    fix: fix uld db

--- db/db.js
+++ db/db.js
@@ -1,6 +1,11 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    user: 'rzzxlumzbkvnyv',
+    password: '822f994c6fb9ae5146f9a57714028d3d3b5a0367da15524eaafd486aedac5d06',
+    host: 'localhost',
+    port: 5432,
+    database: 'd3rt1htds93egi',
     ssl: {
     rejectUnauthorized: false,
   },
