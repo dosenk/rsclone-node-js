581021d977df95dc5d40a541fc414b218c596b8c
Dmitry Osipenko <eympel@gmail.com>
2021-01-08 15:04:59 +0300

    1

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use('/', mainRouter);
 app.use('/users', userRouter);
 app.use('/client', clientRouter);
-
+console.log(process.env.PORT);
 const server = require('http').createServer(app);
 const options = {
     cors: true,
@@ -44,5 +44,5 @@ io.on('connection', socket => {
 });
 
 server.listen(process.env.PORT, () => {console.log(process.env.PORT)});
-console.log(process.env.PORT);
+
 module.exports = app;
