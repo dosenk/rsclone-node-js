4e6cf044ecd660a1935eec26b32a3489fcff5839
Dmitry Osipenko <eympel@gmail.com>
2021-01-08 16:56:10 +0300

    asasas

--- app.js
+++ app.js
@@ -9,39 +9,14 @@ const clientRouter = require('./routes/client.router');
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 // app.use('/', mainRouter);
 app.use('/users', userRouter);
 app.use('/client', clientRouter);
 
-const server = require('http').createServer(app);
-const options = {
-    cors: true,
-    methods: ["GET", "POST"],
-    origins:["https://rsclone-node-js.herokuapp.com/"],
-};
-const io = require('socket.io')(server, options);
 
-const users = new Map();
-
-io.on('connection', socket => { 
-    socket.on("broadcast", (args) => {
-        console.log(args);
-        io.emit("broadcast", users.get(socket.client.id), args);
-    });
-
-    socket.on("name", (args) => {
-        console.log(args);
-        users.set(socket.client.id, args);
-    });
-
-    socket.on("disconnect", () => {
-        console.log('disconnect');
-        users.delete(socket.client.id);
-    });
-});
 // const PORT = process.env.PORT || 3000;
 // console.log(PORT, ' - port HEROKU');
 // server.listen(39287, () => { console.log(PORT, ' - listen') });
