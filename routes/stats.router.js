a44f00e6bc8a192c5b77f235d0d5e9a9f392b6c8
Dmitry Osipenko <eympel@gmail.com>
2021-01-26 17:03:50 +0300

    feat: add PUT query for API

--- routes/stats.router.js
+++ routes/stats.router.js
@@ -3,7 +3,7 @@ const Router = require('express');
 const router = new Router();
 const statsController = require('../controller/stats.controller');
 
-router.post('/setstat', statsController.setUserStats);
+router.put('/setstat', statsController.setUserStats);
 router.get('/stat', statsController.getUserStats);
 router.get('/rating', statsController.getUsersRating);
 
